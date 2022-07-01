1cdc95970cacb536883aad82936b4a111781e6a4
elton1738 <108518023+elton1738@users.noreply.github.com>
2022-07-01 11:29:44 +0300

    Add files via upload

---
+++ structured programming.c
@@ -0,0 +1,38 @@
+/*
+Simple Calculator
+Created by Elton
+DIT-01-8817/2021
+on July, 2022
+C89 compiler
+MIT License
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2,sum,diff,product;
+    float quotient;
+    printf("\tSimple calculator!\n");
+    printf("Enter two numbers: ");
+    scanf("%d%d",&num1,&num2);
+
+    //computations
+    sum = num1 + num2;
+    diff = num1 - num2;
+    product = num1 * num2;
+    quotient = (float)num1/num2;
+
+    //output
+    printf("%d+%d=%d\n",num1,num2,sum);
+    printf("%d-%d=%d\n",num1,num2,diff);
+    printf("%d*%d=%d\n",num1,num2,product);
+    printf("%d/%d=%d\n,"num1,num2,quotient);
+
+
+
+
+
+    return 0;
+}
